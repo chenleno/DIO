51a31e91c6a1625649152a16a6834a98d7ef297b
lenochen <lenochen@foxmail.com>
2020-03-08 17:20:40 +0800

    build: :wrench: webpack配置更新

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,12 +3,19 @@ const paths = require('../tools/paths')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'production',
   entry: {
     index: paths.resolvePath('index.ts')
   },
   output: {
     path: paths.resolvePath('dist'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
+    /**
+     * libraryTarget, 用于表示webpack打包后的文件基于何种方式发挥作用
+     * libraryTarget: 'commonjs2', 入口起点的返回值将分配给 module.exports 对象,
+     * 故可在使用时,使用 "模块引用" 的方式导入
+     */
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [{
@@ -16,10 +23,10 @@ module.exports = {
       use: [
         {
           loader: 'babel-loader',
-          // options: {
-          //   presets: ['@babel/preset-env', { modules: false }], // modules: false  开启tree-shaking时使用
-          //   cacheDirectory: true
-          // }
+          options: {
+            presets: ['@babel/preset-env'],
+            cacheDirectory: true
+          }
         },
         'ts-loader'
       ],
