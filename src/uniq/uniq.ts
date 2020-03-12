71ac035d62da4a523067e239323402d73cb8f28d
lenochen <lenochen@foxmail.com>
2020-03-12 16:49:52 +0800

    feat: :sprkles: uniq 方法新增sortbale参数确定返回值排序方式

--- src/uniq/uniq.ts
+++ src/uniq/uniq.ts
@@ -2,29 +2,35 @@
 interface IUniq<T> {
   /**
    * @param {string | string[] | number[]} props 需要去重的字符串或数组
+   * @param {boolean} sortble 去重后是否需要重新排序,默认为true
    */
-  (props: string | T[] ): string | T[]
+  (props: string | T[], sortble?: boolean ): string | T[]
 }
 
 /**
  * 数组,字符串 去重方法,返回值保留原顺序
  */
 let uniq: IUniq<string | number>
-uniq = props => {
+uniq = (props, sortble = true) => {
   if (!props) return
   const obj = {}
-  const newArr = []
   const isString = typeof(props) === 'string'
-  const propsValue = isString ? (<string>props).split('') : (<string[] | number[]>props)
+  const propsValueArr = isString ? (<string>props).split('') : (<string[] | number[]>props)
 
-  propsValue.forEach(i => {
+  propsValueArr.forEach(i => {
     !obj[i] && (obj[i] = i)
   })
-  propsValue.forEach(i => {
-    !newArr.includes(obj[i]) && newArr.push(obj[i])
-  })
 
-  return isString ? newArr.join('') : newArr
+  if (sortble) {
+    const cv = Object.values(obj).sort()
+    return isString ? cv.join('') : cv
+  } else {
+    const newArr = []
+    propsValueArr.forEach(i => {
+      !newArr.includes(obj[i]) && newArr.push(obj[i])
+    })
+    return isString ? newArr.join('') : newArr
+  }
 }
 
 export default uniq
